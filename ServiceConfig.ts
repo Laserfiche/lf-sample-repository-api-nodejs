635e520088c5af682f8d477bde28e50e2a9df71e
Steven Xu <steven.xu@laserfiche.com>
2022-06-03 18:10:35 -0400

    use base64 string

--- ServiceConfig.ts
+++ ServiceConfig.ts
@@ -1,14 +1,12 @@
 import 'dotenv/config';
 import { AccessKey } from '@laserfiche/lf-api-client-core';
-export const servicePrincipalKey: string =
-  process.env.SERVICE_PRINCIPAL_KEY ?? '';
+export const servicePrincipalKey: string = process.env.SERVICE_PRINCIPAL_KEY ?? '';
 if (!servicePrincipalKey) {
   throw new Error(`Unable to load SERVICE_PRINCIPAL_KEY from .env`);
 }
-let accessKey: any = process.env.OAUTH_ACCESS_KEY;
-const reg = /\\\"/g;
-accessKey = accessKey?.replace(reg, '"');
-accessKey = accessKey?.replace('"{', '{');
-accessKey = accessKey?.replace('}"', '}');
-export const OAuthAccessKey: AccessKey = JSON.parse(accessKey ?? '');
+let accessKey: string = process.env.OAUTH_ACCESS_KEY ?? '';
+if (!accessKey){
+  throw new Error(`Unable to load accessKey from .env`);
+}
+export const OAuthAccessKey: AccessKey = JSON.parse(atob(accessKey) ?? '');
 export const repoId: string = process.env.REPOSITORY_ID_1 ?? '';
