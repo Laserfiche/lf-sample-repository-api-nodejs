85f3d396784411b21dad776b9a6222c9649cdc43
Steven Xu <steven.xu@laserfiche.com>
2022-06-05 22:24:04 -0400

    changed variable name to be more readable

--- ServiceConfig.ts
+++ ServiceConfig.ts
@@ -4,9 +4,9 @@ export const servicePrincipalKey: string = process.env.SERVICE_PRINCIPAL_KEY ?? 
 if (!servicePrincipalKey) {
   throw new Error(`Unable to load SERVICE_PRINCIPAL_KEY from .env`);
 }
-let accessKey: string = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY ?? '';
-if (!accessKey){
+let accessKeyBase64: string = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY ?? '';
+export const OAuthAccessKey: AccessKey = JSON.parse(atob(accessKeyBase64) ?? '');
+if (!accessKeyBase64){
   throw new Error(`Unable to load accessKey from .env`);
 }
-export const OAuthAccessKey: AccessKey = JSON.parse(atob(accessKey) ?? '');
 export const repoId: string = process.env.REPOSITORY_ID_1 ?? '';
