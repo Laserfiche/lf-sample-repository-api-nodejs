03fbfa389bd71c7ca84412a1cecff5d5be36ed51
Steven Xu <steven.xu@laserfiche.com>
2022-06-06 14:00:59 -0400

    used base 64 decoder in @laserfiche/lf-js-utils

--- ServiceConfig.ts
+++ ServiceConfig.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { AccessKey} from '@laserfiche/lf-api-client-core';
+import {StringUtils} from '@laserfiche/lf-js-utils';
 export const servicePrincipalKey: string =
   process.env.DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY ?? '';
 if (!servicePrincipalKey) {
@@ -9,5 +10,5 @@ let accessKeyBase64: string = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACC
 if (!accessKeyBase64){
   throw new Error(`Unable to load accessKeyBase64 from .env`);
 }
-export const OAuthAccessKey: AccessKey = JSON.parse(Buffer.from(accessKeyBase64, 'base64').toString() ?? '');
+export const OAuthAccessKey: AccessKey = JSON.parse(StringUtils.base64toString(accessKeyBase64) ?? '');
 export const repoId: string = process.env.DEV_CA_PUBLIC_USE_REPOSITORY_ID_1 ?? '';
