981e6bea551ded0ba06eac8c35e79eb7028a5b68
Steven Xu <steven.xu@laserfiche.com>
2022-06-06 13:22:07 -0400

    resolved comment

--- ServiceConfig.ts
+++ ServiceConfig.ts
@@ -1,13 +1,17 @@
 import 'dotenv/config';
 import { AccessKey} from '@laserfiche/lf-api-client-core';
-export const testServicePrincipalKey: string =
+export const servicePrincipalKey: string =
   process.env.DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY ?? '';
-if (!testServicePrincipalKey) {
+if (!servicePrincipalKey) {
   throw new Error(`Unable to load DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY from .env`);
 }
 let accessKeyBase64: string = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY ?? '';
-export const OauthAccessKey: AccessKey = JSON.parse(Buffer.from(accessKeyBase64, 'base64').toString() ?? '');
 if (!accessKeyBase64){
-  throw new Error(`Unable to load accessKey from .env`);
+  throw new Error(`Unable to load accessKeyBase64 from .env`);
 }
+export const OAuthAccessKey: AccessKey = JSON.parse(Buffer.from(accessKeyBase64, 'base64').toString() ?? '');
 export const repoId: string = process.env.DEV_CA_PUBLIC_USE_REPOSITORY_ID_1 ?? '';
+if (!repoId){
+  throw new Error(`Unable to load repoId from .env`);
+}
+
