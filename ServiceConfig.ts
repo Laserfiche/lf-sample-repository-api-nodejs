b33a47a7dd65a6bc51a9a42e7da5eedaf2672bed
Steven Xu <steven.xu@laserfiche.com>
2022-06-07 13:52:43 -0400

    changed variable names to be generic

--- ServiceConfig.ts
+++ ServiceConfig.ts
@@ -2,13 +2,13 @@ import 'dotenv/config';
 import { AccessKey} from '@laserfiche/lf-api-client-core';
 import {StringUtils} from '@laserfiche/lf-js-utils';
 export const servicePrincipalKey: string =
-  process.env.DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY ?? '';
+  process.env.SERVICE_PRINCIPAL_KEY ?? '';
 if (!servicePrincipalKey) {
-  throw new Error(`Unable to load DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY from .env`);
+  throw new Error(`Unable to load SERVICE_PRINCIPAL_KEY from .env`);
 }
-let accessKeyBase64: string = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY ?? '';
+let accessKeyBase64: string = process.env.ACCESS_KEY ?? '';
 if (!accessKeyBase64){
-  throw new Error(`Unable to load accessKeyBase64 from .env`);
+  throw new Error(`Unable to load ACCESS_KEY from .env`);
 }
 export const OAuthAccessKey: AccessKey = JSON.parse(StringUtils.base64toString(accessKeyBase64) ?? '');
-export const repoId: string = process.env.DEV_CA_PUBLIC_USE_REPOSITORY_ID_1 ?? '';
+export const repoId: string = process.env.REPOSITORY_ID ?? '';
