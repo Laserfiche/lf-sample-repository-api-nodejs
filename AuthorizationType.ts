6e3b487a9032d3dffc56ce4a5c12f3cfd136e900
Samad Paydar <samad.paydar@laserfiche.com>
2023-10-25 10:40:25 -0400

    Add copyright header.

--- AuthorizationType.ts
+++ AuthorizationType.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Laserfiche.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 export enum authorizationTypeEnum {
   CloudAccessKey = 'CLOUD_ACCESS_KEY',
   APIServerUsernamePassword = 'API_SERVER_USERNAME_PASSWORD',
